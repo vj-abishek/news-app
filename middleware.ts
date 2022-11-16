cfdc0fc5116d3f03eb06970deb9bdb010741fa13
Abishek P.Y <abishek@trainn.co>
2022-11-16 21:26:08 +0530

    removed experimental language change

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,11 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const city = request.geo?.city || null;
-
-  if (request.nextUrl.pathname === "/") {
-    if (city === "Chennai") {
-      return NextResponse.rewrite(new URL("/?lang=tamil", request.url));
-    }
-
-    return NextResponse.next();
-  }
+  // const city = request.geo?.city || null;
+  // if (request.nextUrl.pathname === "/") {
+  //   if (city === "Chennai") {
+  //     return NextResponse.rewrite(new URL("/?lang=tamil", request.url));
+  //   }
+  //   return NextResponse.next();
+  // }
 }
