c14f006c951ebf043acaa80fb2d79458bf3aae37
Abishek P.Y <abishek@trainn.co>
2022-11-16 21:22:08 +0530

    experimental language change based on geo

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers);
+  const city = request.geo?.city || null;
 
-  console.log(request.geo);
+  if (request.nextUrl.pathname === "/") {
+    if (city === "Chennai") {
+      return NextResponse.rewrite(new URL("/?lang=tamil", request.url));
+    }
 
-  NextResponse.next();
+    return NextResponse.next();
+  }
 }
