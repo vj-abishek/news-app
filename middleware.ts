dccaee2b38ebb3b04c186b5760900dd42d54533f
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2022-11-16 13:10:23 +0000

    add cors middleware

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+
+  const ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://fingertip-news.vercel.app",
+    "https://www.theprint.me",
+    "https://theprint.me",
+  ];
+
+  const origin = requestHeaders.get("origin");
+  if (origin && ALLOWED_ORIGINS.includes(origin)) {
+    requestHeaders.set("Access-Control-Allow-Origin", origin);
+  }
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+
+  return response;
+}
