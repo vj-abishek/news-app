60d53c7b70d03b6f67aaa00f60d8bd746e463034
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2022-11-19 09:50:28 +0000

    add unit testing package

---
+++ __tests__/homepage.tsx
@@ -0,0 +1,33 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import Header from "../components/header";
+import "@testing-library/jest-dom";
+import {useSession} from "next-auth/react";
+
+jest.mock("next-auth/react", () => {
+  const originalModule = jest.requireActual('next-auth/react');
+  const mockSession = {
+    expires: new Date(Date.now() + 2 * 86400).toISOString(),
+    user: { username: "admin" }
+  };
+  return {
+    __esModule: true,
+    ...originalModule,
+    useSession: jest.fn(() => {
+      return {data: mockSession, status: 'authenticated'}
+    }),
+  };
+});
+
+describe("Home", () => {
+  test("Should have a header", async() => {
+    render(<Header />);
+
+    expect(screen.getByText("News app")).toBeInTheDocument();
+  });
+
+  test("Should have logout button", async() => {
+    render(<Header />);
+
+    expect(screen.getByText("Logout")).toBeInTheDocument();
+  })
+});
