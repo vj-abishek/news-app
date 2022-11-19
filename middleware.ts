c8c0c361d6fdfea6c57fb02a950352238054f82b
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2022-11-19 03:54:04 +0000

    feat: auto regional news based on location

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const city = request.geo?.city || null;
-  console.log(request.geo)
-  // if (request.nextUrl.pathname === "/") {
-  //   if (city === "Chennai") {
-  //     return NextResponse.rewrite(new URL("/?lang=tamil", request.url));
-  //   }
-  //   return NextResponse.next();
-  // }
+  const region = request.geo?.region || null;
+  
+  if (request.nextUrl.pathname === "/auto") {
+    if (region === "TN") {
+      return NextResponse.rewrite(new URL("/?lang=tamil", request.url));
+    }
+  }
 
   if(request.nextUrl.pathname === "/server") {
     return NextResponse.rewrite(new URL(process.env.SERVER_URL || ""));
