62715bf9a9983f7a1cfae392e846ab23f6be94d5
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2022-11-19 03:49:05 +0000

    made server live and added rewrite to frontend

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // const city = request.geo?.city || null;
+  const city = request.geo?.city || null;
+  console.log(request.geo)
   // if (request.nextUrl.pathname === "/") {
   //   if (city === "Chennai") {
   //     return NextResponse.rewrite(new URL("/?lang=tamil", request.url));
   //   }
   //   return NextResponse.next();
   // }
+
+  if(request.nextUrl.pathname === "/server") {
+    return NextResponse.rewrite(new URL(process.env.SERVER_URL || ""));
+  }
 }
